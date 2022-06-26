8e8f2353e0e245bc6bb5c9084104607762e420b1
Chijioke Chika <8pluscreative@gmail.com>
2022-06-26 14:26:58 -0400

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { RiMenuUnfoldFill, RiCloseLine } from 'react-icons/ri'
 import MLogo from "../../assets/MBoard.svg";
 
 import "./navbar.css";
 
 const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false)
+
+	const toggleHandler = () => {
+    setToggleMenu(toggleMenu => !toggleMenu)
+  }
+
   return (
     <div className="mboard__navbar">
       <div className="mboard__navbar-logo">
@@ -15,6 +21,21 @@ const Navbar = () => {
 					<li id="signin"><a href="google.com">Login</a></li>
 					<li id="signup"><a href="google.com">Sign up</a></li>
 				</ul>
+			</div>
+
+			<div className="mboard__navbar-menu">
+				{toggleMenu 
+				? <RiCloseLine color='#fff' size={27} onClick={toggleHandler} /> 
+				: <RiMenuUnfoldFill color='#fff' size={27} onClick={toggleHandler} /> 
+				}
+				{toggleMenu && 
+					<div className="mboard__navbar-menu-links scale-up-center">
+						<ul>
+							<li id="signin"><a href="google.com">Login</a></li>
+							<li id="signup"><a href="google.com">Sign up</a></li>
+						</ul>
+					</div>
+				}
 			</div>
     </div>
   );
